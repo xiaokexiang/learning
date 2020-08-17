00471c3ad599e391e32c173c797a9e24c8e1e376
xiaokexiang <xxiaokexiang@gmail.com>
2020-08-17 17:52:39 +0800

    jvm 垃圾回收器 20%

---
+++ jvm/src/main/java/top/leejay/jvm/gc/Collectors.java
@@ -0,0 +1,12 @@
+package top.leejay.jvm.gc;
+
+/**
+ * @author xiaokexiang
+ */
+public class Collectors {
+
+    public static void main(String[] args) {
+        int[] arr = new int[1024 * 1024];
+        System.gc();
+    }
+}
