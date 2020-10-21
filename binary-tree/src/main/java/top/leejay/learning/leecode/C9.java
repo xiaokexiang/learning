0f0b1398279096f6ebd261c5565c844ec0ac54d9
xiaokexiang <lijie@beyondcent.com>
2020-10-21 11:50:52 +0800

    leetcode 8,9 双指针

---
+++ binary-tree/src/main/java/top/leejay/learning/leecode/C9.java
@@ -0,0 +1,14 @@
+package top.leejay.learning.leecode;
+
+public class C9 {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
